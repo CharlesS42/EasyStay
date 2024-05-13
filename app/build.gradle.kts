ce07507a5ecb53af717a9f6da10bde435b7bf15b
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2024-05-13 19:15:08 -0400

    tried to implement the google api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,15 @@
 plugins {
-    id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.android.application") version "4.2.0"
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+}
+buildscript {
+    repositories {
+        google() // Add Google's Maven repository
+    }
+    dependencies {
+        classpath ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
 }
 
 android {
@@ -45,8 +53,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("com.google.maps.android:android-maps-utils:0.3+")
 }
--- app/src/main/java/com/example/easystay/MainActivity.kt
+++ app/src/main/java/com/example/easystay/MainActivity.kt
@@ -5,22 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var btnLogIn = findViewById<Button>(R.id.btnLogin)
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
 
-        btnLogIn.setOnClickListener {
-            var deliveryStr: String = "Default user"
-            if (findViewById<EditText>(R.id.edUsername).text.equals("pwd")) {
-                deliveryStr = (findViewById<EditText>(R.id.edUsername).text.toString())
+            var btnLogIn = findViewById<Button>(R.id.btnLogin)
+
+            btnLogIn.setOnClickListener {
+                var deliveryStr: String = "Default user"
+                if (findViewById<EditText>(R.id.edUsername).text.equals("pwd")) {
+                    deliveryStr = (findViewById<EditText>(R.id.edUsername).text.toString())
+                }
+                val intent = Intent(applicationContext, MapSearch::class.java)
+                intent.putExtra("message key", deliveryStr)
+                startActivity(intent)
             }
-            val intent = Intent(applicationContext, MapSearch::class.java)
-            intent.putExtra("message key", deliveryStr)
-            startActivity(intent)
         }
     }
 }
