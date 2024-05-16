5eb7d093433f1f7c1989aa4a2f974bd83ac99389
charlesFinalMS <charles.seg42@gmail.com>
2024-05-16 11:08:23 -0400

    Complete pdf creation linked with 1 page per row in the db

--- app/src/main/java/com/example/easystay/Reservations.kt
+++ app/src/main/java/com/example/easystay/Reservations.kt
@@ -40,25 +40,64 @@ class Reservations : AppCompatActivity() {
             // Create a PageInfo object
             val pageInfo = PdfDocument.PageInfo.Builder(595, 842, 1).create()
 
-            // Start a page
-            val page = document.startPage(pageInfo)
+            // initialize the db handler
+            val db = SQLDBHelper(this, null)
 
-            // Get the Canvas object from the page and use it to draw
-            val canvas: Canvas = page.canvas
-            val paint = Paint()
-            paint.textSize = 14f
-            canvas.drawText("Confirmation: You have saved your selection to the hotel.", 20f, 30f, paint)
-            canvas.drawText("Hotel Address: $hotelAddress", 20f, 70f, paint)
-            canvas.drawText("Number of rooms: $numberOfRooms", 20f, 90f, paint)
-            canvas.drawText("Final price: $finalPrice", 20f, 110f, paint)
+            // -- Add new transaction to the database
 
-            canvas.drawText("Credit Card Information:", 20f, 150f, paint)
-            canvas.drawText("Card Holder: $cardHolder", 20f, 170f, paint)
-            canvas.drawText("Card Number: $cardNum", 20f, 190f, paint)
+            db.AddInfo(
+                hotelAddress,
+                numberOfRooms.toInt(),
+                finalPrice.toDouble(),
+                cardNum.text.toString(),
+                cardHolder.text.toString(),
+                expDate.text.toString(),
+                secCode.text.toString().toInt()
+            )
 
-            // Finish the page
-            document.finishPage(page)
+            // -- Get all transactions to add them to 1 page of a pdf
 
+            // need to initialize the cursor
+            val cursor = db.GetInfo()
+            // move the cursor from one record to another
+            cursor!!.moveToFirst()
+
+            if (cursor != null && cursor.moveToFirst()) {
+                val idIndex = cursor.getColumnIndex(SQLDBHelper.ID_COL)
+                val addressIndex = cursor.getColumnIndex(SQLDBHelper.ADDRESS)
+                val noRoomsIndex = cursor.getColumnIndex(SQLDBHelper.NO_ROOMS)
+                val priceIndex = cursor.getColumnIndex(SQLDBHelper.PRICE)
+                val noCardIndex = cursor.getColumnIndex(SQLDBHelper.NO_CARD)
+                val cardholderIndex = cursor.getColumnIndex(SQLDBHelper.CARDHOLDER)
+
+                do {
+                    // Start a page
+                    val page = document.startPage(pageInfo)
+
+                    // Get the Canvas object from the page and use it to draw
+                    val canvas: Canvas = page.canvas
+                    val paint = Paint()
+                    paint.textSize = 14f
+                    canvas.drawText(
+                        "Confirmation: You have saved your selection to the hotel.",
+                        20f,
+                        30f,
+                        paint
+                    )
+                    canvas.drawText("Hotel Address: $addressIndex", 20f, 70f, paint)
+                    canvas.drawText("Number of rooms: $noRoomsIndex", 20f, 90f, paint)
+                    canvas.drawText("Final price: $priceIndex", 20f, 110f, paint)
+
+                    canvas.drawText("Credit Card Information:", 20f, 150f, paint)
+                    canvas.drawText("Card Holder: $cardholderIndex", 20f, 170f, paint)
+                    canvas.drawText("Card Number: $noCardIndex", 20f, 190f, paint)
+
+                    // Finish the page
+                    document.finishPage(page)
+                } while (cursor.moveToNext())
+                cursor.close()
+
+            }
             // Write the document contents to a file
             val filePath = getExternalFilesDir(null)?.absolutePath + "/confirmation.pdf"
             try {
